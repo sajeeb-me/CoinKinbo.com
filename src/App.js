2514d339ebfb987158d309b5ea1c6c738f3096a2
sdshuvo98 <hellosdshuvo@gamil.com>
2022-06-03 23:42:37 +0600

    designed homepage with video background and little bit animation

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import Navbar from './components/Navbar';
 import { publicRoutes } from './routes/publicRoutes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
+
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  })
   return (
     <div>
       <Navbar>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     const navigate = useNavigate();
 
     return (
-        <div className='flex items-center justify-center h-full'>
+        <div className='text-white'>
             <video autoPlay loop muted
                 style={{
                     position: 'absolute',
@@ -21,9 +21,45 @@ const Home = () => {
             >
                 <source src={Video} type='video/mp4' />
             </video>
-            <div className='App'>
-                <h1 className='text-5xl font-bold'>Welcome to CoinKinbo</h1>
-                <button onClick={() => navigate('/coins')} className='my-4 btn btn-primary btn-outline'>Explore Coins</button>
+            <div className='py-14 px-4 lg:px-20'>
+                <div
+                    data-aos="zoom-out"
+                    data-aos-delay="3000"
+                    data-aos-duration="2000"
+                    className='flex justify-end'>
+                    <div className='bg-accent p-5 bg-opacity-20 rounded-lg w-52'>
+                        <p className='font-semibold'>Today's note:</p>
+                        <div className='text-sm'>
+                            <p className='my-2'>Bitcoin bounces to $30.7K as analyst presents Stock-to-Flow BTC price model rehash.</p>
+                            <a
+                                href="https://cointelegraph.com/news/bitcoin-bounces-to-30-7k-as-analyst-presents-stock-to-flow-btc-price-model-rehash"
+                                target='_blank'
+                                rel="noreferrer"
+                            >
+                                Read now
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className='absolute bottom-20'>
+                    <div
+                        data-aos="fade-right"
+                        data-aos-delay="500"
+                        data-aos-duration="2000"
+                        className='text-3xl lg:text-5xl font-semibold space-y-3 tracking-wide'>
+                        <p>Buy top traded</p>
+                        <p>Cryptocurrencies</p>
+                        <p>from us!</p>
+                    </div>
+                    <button
+                        data-aos="zoom-in"
+                        data-aos-delay="2200"
+                        data-aos-duration="2000"
+                        onClick={() => navigate('/coins')}
+                        className='my-4 btn btn-accent tracking-wider'>
+                        Explore Coins now
+                    </button>
+                </div>
             </div>
         </div >
     );
