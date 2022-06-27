8b09b22cebd6c4cf32c56cccf5e470564528181e
sdshuvo98 <hellosdshuvo@gmail.com>
2022-06-27 23:50:15 +0600

    user protfolio

--- src/pages/Dashboard/MyPortfolio.js
+++ src/pages/Dashboard/MyPortfolio.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../authentication/firebase.init';
 
 const MyPortfolio = () => {
+    const [user] = useAuthState(auth)
+    // console.log(user);
     return (
-        <div>
-            <h1>this is my portfolio</h1>
-        </div>
+        <section className='px-4'>
+            <article className='text-center text-xl'>
+                <h1>Welcome to the portfolio</h1>
+                <h1><span>of </span><span className='text-4xl uppercase font-bold text-primary-focus'>{user.displayName}</span></h1>
+                <p>Let's make your portfolio enrich with more world revolutionary coin's!</p>
+            </article>
+        </section>
     );
 };
 
